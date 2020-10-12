892a82771cb0e37396d18d9cc0b2278393c9c8f2
Davi Félix <davi.felix.lima@gmail.com>
2020-10-12 16:55:07 -0300

    FRONTEND: initial map setup

--- frontend/src/pages/OrphanagesMap/index.tsx
+++ frontend/src/pages/OrphanagesMap/index.tsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import { Map, TileLayer } from 'react-leaflet';
+
+import mapMarkerImg from '../../images/map-marker.svg'
+
+import 'leaflet/dist/leaflet.css'
+
+import '../../styles/pages/orphanages-map.css'
 
 const OrphanagesMap: React.FC = () => {
   return (
-    <h1>Orphanages Map</h1>
+    <div id="page-map">
+
+      <aside>
+
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </footer>
+
+      </aside>
+
+      <Map
+        center={[-23.5668582, -46.660879]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
+      <Link to="/" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+
+    </div>
   );
 }
 
