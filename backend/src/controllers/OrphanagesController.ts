fe567ec4faf80bd9ade43aab95981e7c6494ff44
Davi Félix <davi.felix.lima@gmail.com>
2020-10-13 12:20:01 -0300

    BACKEND: orphanages listing

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -4,6 +4,20 @@ import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 
 export default {
+  async index(req: Request, res: Response) {
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find()
+
+    return res.status(200).json({
+      data: orphanages,
+      message: 'Orphanage found successfully',
+      status: 200,
+    })
+
+  },
+
   async create(req: Request, res: Response) {
 
     const {
