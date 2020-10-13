3ac3b940abe549cc499f5ec72f7019d23871635d
Davi Félix <davi.felix.lima@gmail.com>
2020-10-13 14:07:10 -0300

    BACKEND: validating orphanage creation data

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -5,6 +5,8 @@ import Orphanage from '../models/Orphanage';
 
 import orphanagesView from '../views/orphanages_view'
 
+import * as Yup from 'yup'
+
 export default {
   async index(req: Request, res: Response) {
 
@@ -59,7 +61,7 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       opening_hours,
       latitude,
@@ -68,7 +70,26 @@ export default {
       instructions,
       open_on_weekends,
       images,
-    });
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(Yup.object().shape({
+        path: Yup.string().required()
+      }))
+    })
+
+    await schema.validate(data, {
+      abortEarly: false
+    })
+
+    const orphanage = orphanagesRepository.create(data);
 
     await orphanagesRepository.save(orphanage);
 
--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,8 +1,24 @@
 import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup'
 
+interface ValidationErrors {
+  [key: string]: string[]
+}
 
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   console.error(error)
+  if (error instanceof ValidationError) {
+    let errors: ValidationErrors = {}
+    error.inner.forEach(err => {
+      errors[err.path] = err.errors
+    })
+    return res.status(400).json({
+      data: errors,
+      message: 'You have sent invalid data',
+      status: 400,
+    })
+  }
+
   return res.status(500).json({
     message: 'Internal server error',
     status: 500
