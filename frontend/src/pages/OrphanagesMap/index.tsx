22a0531e1f6cf977746f46667ca4d952dd8adeb4
Davi Félix <davi.felix.lima@gmail.com>
2020-10-14 13:00:04 -0300

    FRONTEND: static map marker on popup

--- frontend/src/pages/OrphanagesMap/index.tsx
+++ frontend/src/pages/OrphanagesMap/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet'
 
 import mapMarkerImg from '../../images/map-marker.svg'
 
 import 'leaflet/dist/leaflet.css'
-
 import '../../styles/pages/orphanages-map.css'
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconAnchor: [29, 68],
+  iconSize: [58, 68],
+  popupAnchor: [170, 5]
+})
 
 const OrphanagesMap: React.FC = () => {
   return (
@@ -33,6 +40,17 @@ const OrphanagesMap: React.FC = () => {
         zoom={15}
         style={{ width: '100%', height: '100%' }}
       >
+        <Marker
+          icon={mapIcon}
+          position={[-23.5668582, -46.660879]}
+        >
+          <Popup closeButton={false} minWidth={248} maxWidth={248} className="map-popup">
+            <span>Lar da Alegria</span>
+            <Link to="">
+              <FiArrowRight size={20} color="#fff" />
+            </Link>
+          </Popup>
+        </Marker>
         <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       </Map>
 
