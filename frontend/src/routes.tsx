26fc82da973570dbda8e1471f55da33459e8c8a0
Davi Félix <davi.felix.lima@gmail.com>
2020-10-14 15:35:47 -0300

    FRONTEND: detail and registration structure

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,13 +3,18 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
+import CreateOrphapage from './pages/CreateOrphanage';
+import Orphanage from './pages/Orphanage';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route name="landing" path='/' component={Landing} exact />
-        <Route name="map" path='/app' component={OrphanagesMap} exact />
+        <Route name="map" path='/app' component={OrphanagesMap} />
+        <Route name="create-orphanage" path='/orphanage/create' component={CreateOrphapage} />
+        <Route name="orphanage" path='/orphanage/:id' component={Orphanage} />
+        <Route name="not-found" component={() => <div>Página não encontrada</div>} />
       </Switch>
     </BrowserRouter>
   );
